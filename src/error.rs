91f45e0680d9c70caa13c5b239be988087a7f93f
Owusu Kelvin Clark <46226886+lokaimoma@users.noreply.github.com>
2024-01-04 22:40:36 +0100

    Update error.rs
    Remove unused error variants

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,4 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum PerceptronError {
-    #[error("{0}")]
-    MisMatchLength(String),
-    #[error("Number of interations greater than number train instances")]
-    NIterLenError,
 }
